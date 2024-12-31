973fe886c7331fcdee98066dcb390c879eaa8474
lonewolfyx <877085893@qq.com>
2024-12-31 16:40:47 +0800

    docs: 更新注释

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -1,3 +1,7 @@
+/**
+ * 判断是否为对象
+ * @param obj
+ */
 export const isObject = (obj: any): obj is Record<string, any> => {
     return obj !== null && typeof obj === 'object' && !Array.isArray(obj);
 }
